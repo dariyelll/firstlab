cecb51a5eeae9a525625260a4525c092bfe9ac8c
unknown <429193-21>
2023-01-10 10:35:53 +0300

    secondcommit2

--- 4/4/Form2.cs
+++ 4/4/Form2.cs
@@ -20,6 +20,7 @@ namespace _4
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int x;
             if (colorDialog1.ShowDialog() == DialogResult.OK)
             {
                 fmain.MyColor2 = button1.ForeColor = colorDialog1.Color;
